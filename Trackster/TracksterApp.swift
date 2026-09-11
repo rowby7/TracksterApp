239781e4886ea7edd1585eb17d67227268bd8289
Rowby Villanueva <116034103+rowby7@users.noreply.github.com>
2026-09-10 23:34:11 -0400

    Initial Commit

---
+++ Trackster/TracksterApp.swift
@@ -0,0 +1,32 @@
+//
+//  TracksterApp.swift
+//  Trackster
+//
+//  Created by Rowby Villanueva on 9/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TracksterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
